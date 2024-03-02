32fca56e174302c41486c5206c6c8d6e8a5dae47
pavelski01 <pavelski01@gmail.com>
2024-03-02 19:28:07 +0100

    Unrary

--- client/Program.cs
+++ client/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dummy;
+using Greet;
+using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,35 @@ namespace client
 {
     internal class Program
     {
+        const string target = "127.0.0.1:50051";
+
         static void Main(string[] args)
         {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+            });
+            //var client = new DummyService.DummyServiceClient(channel);
+            var client = new GreetingService.GreetingServiceClient(channel);
+            var greeting = new Greeting
+            {
+                FirstName = "Bruce",
+                LastName = "Wayne"
+            };
+            var request = new GreetingRequest
+            {
+                Greeting = greeting
+            };
+            var response = client.Greet(request);
+            Console.WriteLine(response.Result);
+            
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
         }
     }
 }
--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Greet;
+using Grpc.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,36 @@ namespace server
 {
     internal class Program
     {
+        const int Port = 50051;
+
         static void Main(string[] args)
         {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on the port: " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine("The server failed to start: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
         }
     }
 }
